8351927e37c9511a48621956f8a50b66139af129
Ana Lilia Ramos García <l01364349@L01364349M01.local>
2019-09-17 20:25:46 -0500

    Add tarea@

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,47 +1,7 @@
 import UIKit
 
-let str = "Hello, playground"
-var string = "Hola"
-
-var stringVar: String!
-
-var darta: String = "Inicializando"
-var simplify = "Inicializando"
-
-var entero:Int!
-var enter64: Int64!
-
-var doble: Double!
-var doblers: Double = 0.0
-
-var casting = Int(doblers)
-
-var operacion = 2.0 * 3.0
-
-var array = [Double]()
-var arrayError: [Double]!
-
-var arrayTodo = [Any]()
-
-var dictionary = [String:Any]()
-
-var booleano = true
-
-var range = 0...5
-for i in range{
-    print(i)
+func fib(_ n: Int) -> Int {
+    guard n > 1 else { return n }
+    return fib(n-1) + fib(n-2)
 }
-for element in arrayTodo{
-    print(element)
-}
-
-var stringLargo = """
-hola esto
-es un string largo
-"""
-
-print("Esto es un string: \(stringLargo)")
-
-func nuevaFuncion(_ parametro: Int) -> Int{
-    return 0
-}
+fib(8)
