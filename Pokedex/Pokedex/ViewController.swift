a1b8c93a4aec322a6eb12b64fc6469ff7a584eed
Ana Lilia Ramos García <l01364349@L01364349M01.local>
2019-09-17 19:29:31 -0500

    init commit

---
+++ Pokedex/Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Ana Lilia Ramos García on 9/17/19.
+//  Copyright © 2019 ITESM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
